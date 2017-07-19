9e5138651346f8f48be9caece08a45eb001a45a1
DogeSWL <lusen.w1387@gmail.com>
2017-07-19 12:52:11 -0700

    modified app and removied config

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config.from_object('config')
+
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI']  = os.environ.get('DATABASE_URL', 'mysql+pymysql://blogz2:MyNewPass@localhost:8889/blogz2')
+app.config['SQLALCHEMY_ECHO'] = True
+
+SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
 
 db = SQLAlchemy(app)
--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DEBUG = True
-
-SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL', 'mysql+pymysql://blogz2:MyNewPass@localhost:8889/blogz2')
-SQLALCHEMY_ECHO = True
-
-SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
