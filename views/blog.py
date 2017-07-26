759747175408041698d51519e0c26667fa2d9fb8
DogeSWL <lusen.w1387@gmail.com>
2017-07-25 21:07:53 -0700

    modified blog.py

--- views/blog.py
+++ views/blog.py
@@ -6,9 +6,7 @@ from models import User, Blog
 def get_blogData_all():
     blog = Blog.query.all()
     user = User.query.all()
-    return db.engine.execute('''SELECT user.id AS id, user.username AS username, blog.id as blog_id, blog.title as title, blog.body as body
-                                FROM blog
-                                LEFT JOIN user ON user.id = blog.owner_id''')
+    return db.engine.execute("SELECT user.id AS id, user.username AS username, blog.id as blog_id, blog.title as title, blog.body as body FROM blog LEFT JOIN user ON user.id = blog.owner_id")
 
 def checkSession():
     if session:
