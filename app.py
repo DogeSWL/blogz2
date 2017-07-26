0b75b34ab641f6d9f50b1b0d2467ee861ab4305b
DogeSWL <lusen.w1387@gmail.com>
2017-07-25 20:29:57 -0700

    modified sk in app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app.config['DEBUG'] = True
 
 app.config['SQLALCHEMY_DATABASE_URI']  = os.environ.get('DATABASE_URL', 'mysql+pymysql://blogz2:MyNewPass@localhost:8889/blogz2')
 app.config['SQLALCHEMY_ECHO'] = True
-
-SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
+app.config['SECRET_KEY'] = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
 
 db = SQLAlchemy(app)
